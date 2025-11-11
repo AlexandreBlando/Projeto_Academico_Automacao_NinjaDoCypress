a975e870da561a930e28fded9b5006d02cc7966c
Blando Alexandre <blandoalexandress@hotmail.com>
2025-11-11 17:00:50 -0300

    Validação CypressCloud

---
+++ petlov/cypress.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  projectId: "14mm92",
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    baseUrl: 'https://petlov.vercel.app'
+  },
+};
